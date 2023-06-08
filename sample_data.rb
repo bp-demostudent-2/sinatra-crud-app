296ae9e15dd7c108219f712c21c25fac39aa0f99
bp-demostudent-2 <132853437+bp-demostudent-2@users.noreply.github.com>
2023-06-08 22:21:14 +0000

    sample data

---
+++ sample_data.rb
@@ -0,0 +1,13 @@
+require './config/environment'
+
+Task.delete_all
+
+count = 0
+while count < 10 do
+  Task.create!(name: "name#{count}", description: "description#{count}")
+  count += 1
+end
+
+Task.all.each do |task|
+  puts task.name
+end
